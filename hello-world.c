8f9746904316fa3d0f9aee35944ac990d6d483ec
Lorenzo <smemo23@gmail.com>
2020-11-13 16:29:12 +0100

    Add function to count up to x

--- hello-world.c
+++ hello-world.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+void printUpToX(int x){
+
+    for(int i=0; i<=x; i++){
+
+        printf("\n%d", i);
+
+    }
+
+}
+
 int main(){
 
 	printf("Hello World");
+        printUpToX(5);
 
 }
